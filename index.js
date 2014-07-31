fabf6591a22b1ef5463cbd0236d4daa088d487e6
Chris Zubak-Skees <chriszs@gmail.com>
2014-07-30 20:18:03 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var fs = require('fs'),
+    natural = require('natural'),
+    _ = require('lodash'),
+    queue = require('queue-async');
+
+var TfIdf = natural.TfIdf;
+var tfidf = new TfIdf();
+var tokenizer = new natural.WordTokenizer();
+
+var basePath = '../srccon-data/2014/transcripts/';
+
+fs.readdir(basePath, function (err,files) {
+
+    var q = queue(1);
+
+    files.forEach(function (file) {
+        q.defer(fs.readFile,basePath + file,{ encoding: 'utf8' });
+    });
+
+    q.awaitAll(function (err,contents) {
+        contents.forEach(function (content) {
+            tfidf.addDocument(content);
+        });
+
+        var result = '';
+
+        contents.forEach(function (content,i) {
+            result += ('\n\n ## [' + files[i] + '](https://github.com/ryanpitts/srccon-data/blob/master/2014/transcripts/' + files[i] + ')\n');
+
+            result += (_.pluck(tfidf.listTerms(i).slice(0,40),'term').join(', '));
+        });
+
+        fs.writeFile(' README.md', result, function (err) {
+            console.log('written');
+        });
+    });
+
+});
